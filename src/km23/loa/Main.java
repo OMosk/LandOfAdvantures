f91a9c369d31d8f4573bf8060bda040a8b67793b
TarasRudnyk <TarasRudnyk@gmail.com>
2014-04-13 14:14:14 +0300

    1 коммит

--- src/km23/loa/Main.java
+++ src/km23/loa/Main.java
@@ -1,9 +1,9 @@
 package km23.loa;
-import org.java_websocket.server.WebSocketServer;
+//import org.java_websocket.server.WebSocketServer;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
+        System.out.println("Hello");
     }
 }
