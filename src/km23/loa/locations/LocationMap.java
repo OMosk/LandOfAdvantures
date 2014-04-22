a9e58221faaec20ecce254280f52a36fba85ebb4
Oleksiy Moskal <mrdefender314@gmail.com>
2014-04-22 21:13:54 +0300

    LocationMap - початкова структура класу

--- src/km23/loa/locations/LocationMap.java
+++ src/km23/loa/locations/LocationMap.java
@@ -4,4 +4,16 @@ package km23.loa.locations;
  * Created by mosk on 13.04.14.
  */
 public class LocationMap {
+    protected Byte[][] backgroundLayer;
+    protected Boolean[][] accessLayer;
+    protected Byte[][] gameplayLayer;
+    protected Byte[][] decorationLayer;
+    protected int tileSize;
+    protected int width, height;
+    public LocationMap(String location){
+
+    }
+
+
+
 }
