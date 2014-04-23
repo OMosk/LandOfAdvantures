7f435123f8e668b9f4798baff8a5984763743eab
Julia <juliesergienko@gmail.com>
2014-04-23 16:40:07 +0300

    Renamed fields.

--- src/km23/loa/characters/BaseCharacter.java
+++ src/km23/loa/characters/BaseCharacter.java
@@ -11,17 +11,17 @@ public class BaseCharacter {
     private int y;
     private ArrayList<km23.loa.characters.effects.BaseEffect> effects;
     private ArrayList<km23.loa.characters.items.Weapon> weapons;
-    private int HP;
-    private int MP;
-    private int Stamina;
-    private int Velocity;
-    private int LVL;
-    private int Armor;
-    private int MelleAttack;
-    private int RangedAttack;
-    private int RegenerationHP;
-    private int RegenerationMP;
-    private int RegenerationStamina;
+    private int healthPoints;
+    private int manaPoints;
+    private int stamina;
+    private int velocity;
+    private int level;
+    private int armor;
+    private int melleAttack;
+    private int rangedAttack;
+    private int regenerationHealthPoints;
+    private int regenerationManaPoints;
+    private int regenerationStamina;
 
 
 }
