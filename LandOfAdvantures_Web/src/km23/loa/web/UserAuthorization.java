1bc63fba8c3da261b24c60075b06905c2ba24569
Julia <juliesergienko@gmail.com>
2014-05-20 18:56:30 +0300

    Implemented authorization

--- LandOfAdvantures_Web/src/km23/loa/web/UserAuthorization.java
+++ LandOfAdvantures_Web/src/km23/loa/web/UserAuthorization.java
@@ -21,68 +21,78 @@ public class UserAuthorization {
     public boolean checkUserLoginPassword(String login, String password){
         boolean result = false;
         try{
-            ResultSet users = LOADataBaseConnection.query("SELECT * FROM `users` WHERE `user` = '" + login + "' AND `password_hash` = '" + getMD5Hash(password) + "'");
-            users.last();
-            if(users.getRow()>=1)result = true;
-            users.beforeFirst();
+            ResultSet users = LOADataBaseConnection.query("SELECT * FROM `users` WHERE `user` = '" + login + "' AND `password_hash` = '" + getMD5Hash(password) + "' LIMIT 1");
+            if(users.next())result = true;
         }
         catch (SQLException e){
-            Logger.log(e.getMessage());
+            //Logger.log(e.getMessage());
+            e.printStackTrace();
         }
         return result;
     }
     public void authorizeUser(String login, String session_id, String session_hash){
         try{
-            LOADataBaseConnection.query("UPDATE `users` SET `session_id` = '" + session_id + "', 'session_hash' = '" + session_hash + "' WHERE `user` = '" + login + "' LIMIT 1");
+            LOADataBaseConnection.queryUpdateData("UPDATE `users` SET `session_id` = '" + session_id + "', `session_hash` = '" + session_hash + "' WHERE `user` = '" + login + "' LIMIT 1");
             this.login = login;
             authorized = true;
         }
         catch (SQLException e){
             Logger.log(e.getMessage());
+            e.printStackTrace();
         }
     }
     //public boolean checkUserAu
     protected String getMD5Hash(String string){
         String md5Hash = null;
         byte[] stringBytes = null;
+
         try {
             stringBytes = string.getBytes("UTF-8");
-        }
-        catch(UnsupportedEncodingException e){
-            e.printStackTrace();
-        }
-        try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] hashDigits = md.digest(stringBytes);
             StringBuilder sb = new StringBuilder(2*hashDigits.length);
             for(byte b : hashDigits){
                 sb.append(String.format("%02x", b&0xff));
             }
-
             md5Hash = sb.toString();
-//            try {
-//                //md5Hash = new String(hashDigits, "UTF-8");
-//
-//
-//
-//            }
-//            catch(UnsupportedEncodingException e){
-//                e.printStackTrace();
-//            }
         }
         catch (NoSuchAlgorithmException e){
-            //Logger.log(e.getMessage());
             e.printStackTrace();
         }
-        System.out.println("md5 " + string + " =" + md5Hash);
-        System.out.println("md5 " + string + " =");
+        catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
         return md5Hash;
     }
     public boolean isAuthorized() {
         return authorized;
     }
+    public boolean checkUserAuthorization(String sessionId, String sessionHash){
+        try {
+            ResultSet resultSet = LOADataBaseConnection.query("SELECT * FROM `users` WHERE `session_id`='" + sessionId + "' AND `session_hash`='" + sessionHash + "' LIMIT 1");
+            if(resultSet.next()){
+                authorized = true;
+                login = resultSet.getString("user");
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return authorized;
+    }
 
     public String getLogin() {
         return login;
+    }
+
+    public void logout(){
+        if(login!=null){
+            try {
+                LOADataBaseConnection.queryUpdateData("UPDATE `users` SET `session_id`='', `session_hash`='' WHERE `user`='" + login + "'");
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
--- LandOfAdvantures_Web/src/km23/loa/web/servlets/AuthorizationServlet.java
+++ LandOfAdvantures_Web/src/km23/loa/web/servlets/AuthorizationServlet.java
@@ -3,24 +3,89 @@ package km23.loa.web.servlets;
 /**
  * Created by mosk on 15.05.14.
  */
+import km23.loa.web.UserAuthorization;
+import km23.loa.web.util.GlobalParameters;
+
 import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 
 public class AuthorizationServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-//        // Set response content type
-//        response.setContentType("text/html");
-//        // Actual logic goes here.
-//        PrintWriter out = response.getWriter();
-//        out.println("<h1>" + message + "</h1>");
-        //RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/WEB-INF/jsp/registration.jsp");
-        //requestDispatcher.forward(request, response);
-        response.setContentType("text/html");
-        PrintWriter out =  response.getWriter();
-        out.println("Hello world");
-        out.flush();
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        if(request.getParameter("do").equals("logout")){
+
+            GlobalParameters.setServletContext(getServletContext());
+            UserAuthorization userAuthorization = new UserAuthorization();
+
+            String session_hash = null;
+
+            Cookie[] cookies = request.getCookies();
+            for(Cookie cookie: cookies){
+                if(cookie.getName().equals("session_hash")) session_hash = cookie.getValue();
+            }
+            userAuthorization.checkUserAuthorization(request.getSession().getId(), session_hash);
+            userAuthorization.logout();
+
+            response.sendRedirect(request.getHeader("referer")!=null?request.getHeader("referer"): "http://"+request.getServerName() + ":" + request.getLocalPort()  + getServletContext().getContextPath() + "/");
+            System.out.println("http://"+request.getServerName() + ":" + request.getLocalPort()  + getServletContext().getContextPath() + "/");
+
+        }
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        GlobalParameters.setServletContext(getServletContext());
+        UserAuthorization userAuthorization = new UserAuthorization();
+
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        if(login!=null && password!=null){
+            if(userAuthorization.checkUserLoginPassword(login, password)){
+                String sessionId = request.getSession().getId();
+                String sessionHash = getMD5Hash( String.valueOf(System.currentTimeMillis()) );
+                userAuthorization.authorizeUser(login, sessionId, sessionHash);
+                if(userAuthorization.checkUserAuthorization(sessionId, sessionHash)){
+                    Cookie sessionHashCookie = new Cookie("session_hash", sessionHash);
+                    response.addCookie(sessionHashCookie);
+                    //getServletContext().getRequestDispatcher("/-.jsp").forward(request, response);
+                    response.sendRedirect(request.getHeader("referer"));
+                }
+                else {
+                    response.sendRedirect(request.getHeader("referer"));
+                }
+            }
+            else {
+                request.setAttribute("wrong_loginpassword", true);
+                getServletContext().getRequestDispatcher("/WEB-INF/jsp/authorization.jsp").forward(request, response);
+            }
+        }
+        else{
+            response.sendRedirect(request.getHeader("referer"));
+        }
+    }
+    protected String getMD5Hash(String string){
+        String md5Hash = null;
+        byte[] stringBytes = null;
+
+        try {
+            stringBytes = string.getBytes("UTF-8");
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hashDigits = md.digest(stringBytes);
+            StringBuilder sb = new StringBuilder(2*hashDigits.length);
+            for(byte b : hashDigits){
+                sb.append(String.format("%02x", b&0xff));
+            }
+            md5Hash = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        return md5Hash;
     }
 }
